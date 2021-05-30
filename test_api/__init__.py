b17ea9068c6b8fb08f48a22546fd59f56547bddd
wmcgee3 <61711986+wmcgee3@users.noreply.github.com>
2021-05-30 17:29:17 -0400

    linting fixes

--- test_api/__init__.py
+++ test_api/__init__.py
@@ -1,7 +1,12 @@
+"""Creates the Flask application."""
+
 import flask
 
 
 def create_app():
+    """Create the Flask app."""
+    # pylint: disable=import-outside-toplevel
+
     app = flask.Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
--- test_api/models.py
+++ test_api/models.py
@@ -1,3 +1,5 @@
+"""Models for api persistent data."""
+
 import json
 
 import flask_sqlalchemy
@@ -6,6 +8,9 @@ db = flask_sqlalchemy.SQLAlchemy()
 
 
 class Application(db.Model):
+    """ORM object for Application."""
+    # pylint: disable=no-member,too-few-public-methods
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), unique=True, nullable=False)
     api_key = db.Column(db.String(64))
