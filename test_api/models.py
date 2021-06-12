5f23aef4b7b90b282aca72d7b8c617b0c12302fa
wmcgee3 <61711986+wmcgee3@users.noreply.github.com>
2021-06-12 00:41:01 -0400

    enable creating and reading messages

--- test_api/models.py
+++ test_api/models.py
@@ -1,25 +1,30 @@
 """Models for api persistent data."""
+# pylint: disable=no-member,too-few-public-methods
 
-import json
+from flask_sqlalchemy import SQLAlchemy
 
-import flask_sqlalchemy
-
-db = flask_sqlalchemy.SQLAlchemy()
+db = SQLAlchemy()
 
 
 class Application(db.Model):
     """ORM object for Application."""
-    # pylint: disable=no-member,too-few-public-methods
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), unique=True, nullable=False)
-    api_key = db.Column(db.String(64))
+    api_key = db.Column(db.String(64), unique=True, nullable=False)
+    messages = db.relationship('Message', backref='application', lazy=True)
 
     def __repr__(self):
-        return json.dumps(
-            {
-                'name': self.name,
-                'api_key': self.api_key
-            },
-            indent=4
-        )
+        return str(self.name)
+
+
+class Message(db.Model):
+    """ORM object for saving messages."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(32), nullable=False)
+    text = db.Column(db.Text(), nullable=False)
+    application_id = db.Column(db.Integer, db.ForeignKey('application.id'), nullable=False)
+
+    def __repr__(self):
+        return str(self.title)
